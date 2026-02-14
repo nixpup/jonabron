9590a1696d213ba41be5dd8a2b4981908c986c2d
nixpup <zweitlaender@gmail.com>
2026-02-14 16:23:47 +0100

    Added Gamemode and Gamemode-Service-Type to Games Definition

--- jonabron/packages/games.scm
+++ jonabron/packages/games.scm
@@ -1,7 +1,14 @@
 (define-module (jonabron packages games)
   #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages)
+  #:use-module (gnu services)
+  #:use-module (gnu packages admin)
+  #:use-module (gnu services dbus)
+  #:use-module (gnu services shepherd)
   #:use-module (gnu packages base)
   #:use-module (gnu packages bash)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages serialization)
   #:use-module (gnu packages bootstrap)
   #:use-module (gnu packages commencement)
   #:use-module (gnu packages compression)
@@ -33,17 +40,19 @@
   #:use-module (gnu packages xdisorg)
   #:use-module (gnu packages xorg)
   #:use-module (guix build utils)
+  #:use-module (gnu packages build-tools)
+  #:use-module (gnu packages ninja)
   #:use-module (guix build-system cargo)
   #:use-module (guix build-system copy)
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system trivial)
+  #:use-module (guix build-system meson)
   #:use-module (guix download)
   #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module (guix packages)
   #:use-module (guix utils)
-  #:export (osu-lazer-bin))
-
+  #:export (osu-lazer-bin gamemode gamemode-service-type))
 
 (define osu-lazer-bin
   (package
@@ -146,7 +155,9 @@
             openssl
             vulkan-loader
             wayland
-            zlib))
+            zlib
+            alsa-plugins
+            pulseaudio))
     (home-page "https://osu.ppy.sh/")
     (synopsis "rhythm is just a *click* away!")
     (description "A free-to-win rhythm game. This is the future – and final
@@ -155,3 +166,99 @@ Currently known by and released under the release codename lazer. As in
 sharper than cutting-edge.")
     (properties '((upstream-name  . "osu")))
     (license license:expat)))
+
+(define-public gamemode
+  (package
+    (name "gamemode")
+    (version "1.8.2")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/FeralInteractive/gamemode")
+             (commit version)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "14i3y385jzpr30lxk38z84rq76sy4cz0pwm54rm62f4mnk0xwjjp"))
+       (patches (list (local-file "files/gamemode-preload-nix-workaround.patch")))))
+    (build-system meson-build-system)
+    (arguments
+     (list
+      #:configure-flags
+      #~(list "-Dwith-pam-limits-dir=etc/security/limits.d"
+              "-Dwith-systemd-user-unit-dir=lib/systemd/user"
+              "-Dwith-systemd-group-dir=lib/sysusers.d"
+              "--libexecdir=libexec")
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'patch-paths
+            (lambda _
+              ;; Fix the library path for the runner script
+              (substitute* "data/gamemoderun"
+                (("@libraryPath@")
+                 (string-append #$output "/lib")))))
+          (add-before 'configure 'fix-systemd-dep
+                      (lambda _
+                        (let ((elogind-pc (string-append #+elogind "/lib/pkgconfig/libelogind.pc")))
+                          (mkdir-p "pkgconfig")
+                          (copy-file elogind-pc "pkgconfig/libsystemd.pc")
+                          (setenv "PKG_CONFIG_PATH"
+                                  (string-append (getcwd) "/pkgconfig:"
+                                                 (or (getenv "PKG_CONFIG_PATH") ""))))))
+          (add-after 'install 'post-fixup
+            (lambda _
+              (let* ((bin-dir (string-append #$output "/bin"))
+                     (lib-dir (string-append #$output "/lib")))
+                (for-each
+                 (lambda (prog)
+                   (let ((file (string-append bin-dir "/" prog)))
+                     (when (file-exists? file)
+                       (invoke "patchelf" "--set-rpath"
+                               (string-append lib-dir ":" (getenv "LIBRARY_PATH"))
+                               file))))
+                 '("gamemoded" "gamemode-simulate-game"))))))))
+(native-inputs
+     (list pkg-config meson ninja patchelf))
+    (inputs
+     (list dbus libinih procps psmisc elogind))
+    (home-page "https://feralinteractive.github.io/gamemode")
+    (synopsis "Optimise Linux System Performance on Demand")
+    (description "GameMode is a daemon/lib combo for Linux that allows games to
+request a set of optimisations be temporarily applied to the host operating
+system and/or a game process.")
+    (properties '((upstream-name  . "gamemode")))
+    (license license:bsd-3)))
+
+(define %gamemode-polkit-rule
+  (file-union "gamemode-polkit"
+              `(("share/polkit-1/rules.d/gamemode.rules"
+                 ,(plain-file "gamemode.rules"
+                              "polkit.addRule(function(action, subject) {
+                              if (action.id == \"com.feralinteractive.GameMode.governor-helper\" ||
+                                  action.id == \"com.feralinteractive.GameMode.procmgr-helper\") {
+                                  return polkit.Result.YES;
+                              }
+                              });")))))
+
+; Enables '(service gamemode-service-type)' to be used in (operating-system (services ... )) definition.
+(define-public gamemode-service-type
+               (service-type
+                 (name 'gamemode)
+                 (extensions (list
+                   (service-extension dbus-root-service-type
+                                      (compose list identity))
+                   (service-extension polkit-service-type
+                                      (lambda (_) (list %gamemode-polkit-rule)))
+
+                   (service-extension shepherd-root-service-type
+                                      (lambda (package)
+                                        (list (shepherd-service
+                                                (provision '(gamemode))
+                                                (documentation "Run the GameMode daemon.")
+                                                (requirement '(dbus user-processes))
+                                                (start #~(make-forkexec-constructor
+                                                           (list #$(file-append package "/bin/gamemoded") "-d")))
+                                                (stop #~(make-kill-destructor))))))))
+                   (default-value gamemode)
+                   (description "Set up GameMode D-Bus and Polkit policies, as well as run the GameMode daemon.")
+                   ))
