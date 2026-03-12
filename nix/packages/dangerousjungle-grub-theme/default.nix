46985ad9741710e109e9c620905cb6ac5aa8cc23
librepup <zweitlaender@gmail.com>
2026-03-12 19:52:41 +0100

    Update version and installation path for theme

--- nix/packages/dangerousjungle-grub-theme/default.nix
+++ nix/packages/dangerousjungle-grub-theme/default.nix
@@ -1,19 +1,22 @@
 { pkgs ? import <nixpkgs> {} }:
+let
+  inherit (pkgs) lib;
+in
 pkgs.stdenv.mkDerivation rec {
   pname = "dangerousjungle-grub-theme";
-  version = "1.0";
+  version = "1.1";
   src = ./theme;
   installPhase = ''
     runHook preInstall
-    mkdir -p $out/
-    cp -r ./* $out/
+    mkdir -p $out/share/grub/themes/${pname}
+    cp -r ./* $out/share/grub/themes/${pname}/
     runHook postInstall
   '';
-  meta = {
+  meta = with lib; {
     description = "Dangerous Jungle (Vibrant) GRUB Bootloader Theme";
     homepage = "https://github.com/librepup/jonabron";
-    license = lib.licenses.gpl3Plus;
-    platforms = lib.platforms.linux;
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
     maintainers = [ maintainers.librepup ];
   };
 }
